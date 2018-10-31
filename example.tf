dab221f04c2d0802543c47c48a5f644e56c62917
Matt Westgate <matt@lullabot.com>
2018-10-31 09:35:13 -0400

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "${aws_access_key}"
-  secret_key = "${aws_secret_key}"
+  access_key = "$${aws_access_key}"
+  secret_key = "$${aws_secret_key}"
   region     = "us-east-1"
 }
 
